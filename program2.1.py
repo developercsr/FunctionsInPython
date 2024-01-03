64ef2b7ede9001b2601517c9f1e26adba34e3279
sumanth <sumanthroyal111@gmail.com>
2024-01-04 02:51:39 +0530

    input function

---
+++ program2.1.py
@@ -0,0 +1,6 @@
+#input() function 
+x=input("Enter input : ")#Takes everything as String
+y=int(input("Enter a number : "))
+z=float(input("Enter a floating number : "))
+a=complex(input("Enter Complex Number : "))
+print(x,y,z,a)
