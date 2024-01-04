35a4b6628222814057544b3fc8c675bc3595c55d
sumanth <sumanthroyal111@gmail.com>
2024-01-04 11:47:49 +0530

    min and max

---
+++ python2.7.py
@@ -0,0 +1,6 @@
+#min and Max
+print(min(5,6,7,7.67,8,8,89,900))
+print(max(4,5,7,90,98,8,7,6,6,))
+print(min("rtee","T5ey","t35t5","5tegerg")) # consider as alphabetical wise
+#Numbers considers as the minimum compared to the alphabets
+print(max("23f4f","fra,","frfer")) # consider alphabetical wise
