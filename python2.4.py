34382318f3efd5b8c0637d4056f3d691bf88619a
sumanth <sumanthroyal111@gmail.com>
2024-01-04 11:10:54 +0530

    type function

---
+++ python2.4.py
@@ -0,0 +1,25 @@
+#Type Function ==> This is useed to know the Data Type of the Object
+class jai:
+    a=23
+    c=35
+hind=jai()
+num=24
+flo=355.5543
+com=33+78j
+boool= True
+str1= "tfytt"
+dic = {"a":"abc","b":"bacd","c":"cad"}
+set1={2,3,5,6,7,8}
+tup=(3,4,5,6,7,8,8)
+lis=[4,78,8,9,9,90,0,0]
+print(type(num))
+print(type(flo))
+print(type(com))
+print(type(boool))
+print(type(str1))
+print(type(dic))
+print(type(set1))
+print(type(tup))
+print(type(lis))
+print(type(jai)) #prints as type for classes
+print(type(hind)) #prints for Objects as ==> '__main__.classname'
