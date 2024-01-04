f15111ad71035555f55754d8cc9dd5d683abb681
sumanth <sumanthroyal111@gmail.com>
2024-01-04 12:12:10 +0530

    math module

---
+++ python2.9.py
@@ -0,0 +1,19 @@
+#math module
+import math
+print(math.ceil(7.65)) 
+print(math.floor(6.77))
+print(math.fabs(-86.878))
+print(math.log(2,10)) #log 2 base 10
+print(math.log10(2))
+print(math.pow(2,3))#2**3
+print(math.sqrt(4)) #4**0.5
+print(math.pi) #pi is a constant value
+print(math.cos(math.pi))
+print(math.sin(math.pi/2))
+print(math.tan(math.pi/4))
+print(math.acos(-1)) # front a means inverse
+print(math.atan(1))
+print(math.asin(0))
+print(math.degrees(math.pi))
+print(math.radians(90))
+
