3432d6c056c489cacc1372b24293e68a703e7b8f
sumanth <sumanthroyal111@gmail.com>
2024-01-18 13:53:30 +0530

    Random Push

---
+++ python2.14.py
@@ -0,0 +1,6 @@
+k=int(input("Enter the input : "))
+for i in range(1,k+1):
+    print(" "*,"*"*(2*i+1))
+
+for i in range(0,k+1):
+    print(" "*(i-1),"*"*(2*1))
--- python2.5.py
+++
@@ -1,3 +0,0 @@
-#round of function ==> used to round the floats to the nerest pointts
-print(round(34.456),round(45.65443,3),round(878584.45666,1))
-print(round(-98.768,1),round(-884.1839744))
